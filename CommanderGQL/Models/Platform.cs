fd3b63f716aad94c1b7cbb0baff884a2462ccee6
clonediff <32036592+clonediff@users.noreply.github.com>
2023-09-20 12:11:26 +0300

    [add] description attribute

--- CommanderGQL/Models/Platform.cs
+++ CommanderGQL/Models/Platform.cs
@@ -2,6 +2,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace CommanderGQL.Models;
 
+[GraphQLDescription("Represents any software or service that has a command line interface.")]
 public class Platform
 {
     [Key]
@@ -10,6 +11,7 @@ public class Platform
     [Required]
     public string Name { get; set; } = default!;
     
+    [GraphQLDescription("Represents a purchased, valid license for the platform.")]
     public string? LicenseKey { get; set; }
 
     public ICollection<Command> Commands { get; set; } = new List<Command>();
